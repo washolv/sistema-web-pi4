38e45c5e311b261d8f796237b284958facaea177
Debora Souza <debora.r.souza@globalhitss.com.br>
2021-04-07 12:36:06 -0300

    classe cliente

--- src/app/modules/cliente/models/Cliente.ts
+++ src/app/modules/cliente/models/Cliente.ts
@@ -1,6 +1,20 @@
 export class Cliente {
-  status?: number;
   id?: number;
   nome?: string;
+  email?: string;
+  senha?: string;
+  telefone?: string;
   cpf?: string;
+  sexo?: string;
+  dataNascimento?: Date;
+  endereco?: Endereco;
+}
+
+export class Endereco {
+  id?: number;
+  cep?: string;
+  logradouro?:string;
+  cidade?:string;
+  uf?:string;
+  t?: string;
 }
