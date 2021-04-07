58efcfe3f31901dedeced748b8c5b194048d719e
Debora Souza <debora.r.souza@globalhitss.com.br>
2021-04-07 12:35:11 -0300

    service cliente

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,14 +12,33 @@ export class ClienteService {
 
   constructor(private http: HttpClient) { }
 
-  public editarCliente(cliente: Cliente): Observable<Cliente>{
-    return this.http.post<Cliente>(`${this.apiUrl}/cliente`, cliente);
-  }
+
   public buscarCliente(id: number): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(`${this.apiUrl}/cliente/${id}`);
+    return this.http.get<Cliente[]>(`${this.apiUrl}/clientes/${id}`);
   }
   public buscarClientes(): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(`${this.apiUrl}/cliente`);
+    return this.http.get<Cliente[]>(`${this.apiUrl}/clientes`);
+  }
+  public salvarCliente(cliente: Cliente): Observable<Cliente[]> {
+    return this.http.post<Cliente[]>(`${this.apiUrl}/clientes`, cliente);
+  }
+  public editarCliente(cliente: Cliente): Observable<Cliente>{
+    return this.http.put<Cliente>(`${this.apiUrl}/clientes`, cliente.nome);
+  }
+  public deleteCliente(id?: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.apiUrl}/clientes/${id}`);
   }
 
+  public emailNaoCadastrado(email: string):Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({'email': email })
+    };
+     return this.http.get<Cliente>(`${this.apiUrl}/usuarios`, httpOptions);
+  }
+  public cpfNaoCadastrado(cpf: string):Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({'cpf': cpf })
+    };
+     return this.http.get<Cliente>(`${this.apiUrl}/funcionarios/cpf`, httpOptions)
+  };
 }
