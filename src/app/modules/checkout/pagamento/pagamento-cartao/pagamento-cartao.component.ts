20407e5b1edc1c1eae690f14ca58a4b220e1774e
Francisco Oliveira <55900260+washolv@users.noreply.github.com>
2021-05-06 20:48:16 -0300

    alteracoes na compra

--- src/app/modules/checkout/pagamento/pagamento-cartao/pagamento-cartao.component.ts
+++ src/app/modules/checkout/pagamento/pagamento-cartao/pagamento-cartao.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagamento-cartao.component.css']
 })
 export class PagamentoCartaoComponent implements OnInit {
-
+  parcelas: any[]= new Array([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12]);
   constructor() { }
 
   ngOnInit() {
